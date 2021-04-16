8e2347e8f1956f2bbb56377506432548104708ae
Kalelovil <kalelovil@gmail.com>
2021-04-17 11:24:35 +1200

    - player input manager added.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
+        transform.SetParent(GameObject.FindGameObjectWithTag("Player_Parent").transform, false);
     }
     void Start()
     {

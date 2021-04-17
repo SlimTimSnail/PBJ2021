b18e3addef9f30ee0a27dd6bc901a28e146d6b34
Kalelovil <kalelovil@gmail.com>
2021-04-17 16:56:29 +1200

    - moved player movement to fixedupdate - reduced player movement speed slightly - increased player size to counteract sprite being visually smaller than its size

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 m_movementLastInputValue;
 
     // Constants
-    private const float MOVEMENT_TO_FORCE_MULTIPLIER = 500f;
+    private const float MOVEMENT_TO_FORCE_MULTIPLIER = 10f;
 
     [SerializeField]
     private bool m_ignoreSpawner;
@@ -34,11 +34,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (m_movementActionHeld && !(m_movementLastInputValue == new Vector2(0, 0)))
         {
-            Vector2 movement = m_movementLastInputValue * m_movementSpeed * MOVEMENT_TO_FORCE_MULTIPLIER * Time.deltaTime;
+            Vector2 movement = m_movementLastInputValue * m_movementSpeed * MOVEMENT_TO_FORCE_MULTIPLIER;
             m_rigidbody.AddForce(movement);
         }
     }

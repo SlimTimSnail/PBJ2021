f65194b8a0610b025ecae6e8961f4c4720cfda81
OlivierYC <olivier.y@chrun.com>
2021-04-17 18:42:57 +1200

    Revert "Revert "Merge branch 'main' of https://github.com/SlimTimSnail/PBJ2021 into main""
    This reverts commit 4dc9b223fa7a817106b019b877b14d03c7361cb3.

--- Assets/Scripts/WordMovement.cs
+++ Assets/Scripts/WordMovement.cs
@@ -9,6 +9,12 @@ public class WordMovement : MonoBehaviour
     [SerializeField]
     private bool m_ignoreSpawner;
 
+    [SerializeField]
+    private Vector2 m_constantForce;
+
+    [SerializeField]
+    private bool m_randomStartingRotation;
+
     private void Awake()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
@@ -21,5 +27,16 @@ public class WordMovement : MonoBehaviour
         {
             transform.position = GameController.Instance.GetWordSpawnPosition();
         }
+
+        if (m_randomStartingRotation)
+        {
+            float randomRotation = UnityEngine.Random.Range(0f, 360f);
+            transform.eulerAngles = new Vector3(0f, 0f, randomRotation);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidbody.AddForce(m_constantForce);
     }
 }

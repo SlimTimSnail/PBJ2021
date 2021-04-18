c1c7f55eb07c8baebf47f76a2323273250ca37ba
Tim Donnan <t.donnan@live.com.au>
2021-04-18 14:03:47 +1200

    Added a start screen because why not

---
+++ Assets/Scripts/Tim/StartScreen.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+
+    public void OnStartClick()
+    {
+        Invoke("StartGame", 0.3f);
+    }
+
+    private void StartGame()
+	{
+		SceneManager.LoadScene("FinalScene");
+	}
+
+    public void OnExitClick()
+	{
+		Invoke("ExitGame", 0.5f);
+	}
+
+    private void ExitGame()
+	{
+		Application.Quit();
+
+    #if UNITY_EDITOR
+		Debug.LogWarning("Would quit in actual game executable");
+    #endif
+	}
+
+}

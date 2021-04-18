b537fc9a54f4184a65deda7a7606d17ffc3755ac
NevEagleson <neveagleson@gmail.com>
2021-04-18 14:22:54 +1200

    questions

---
+++ Assets/Scripts/DateQuestion.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DateQuestion : MonoBehaviour
+{
+    [SerializeField]
+    private TextToSpeech m_textToSpeech;
+    [SerializeField]
+    private Text m_text;
+
+    void Start()
+    {
+        GameController.Instance.SentenceManager.NewSentenceEvent += OnNewSentence;
+        OnNewSentence(GameController.Instance.SentenceManager.CurrentSentence.Question);
+    }
+
+    private void OnDestroy()
+    {
+        if (GameController.Instance != null)
+        {
+            GameController.Instance.SentenceManager.NewSentenceEvent -= OnNewSentence;
+        }
+    }
+
+    // Update is called once per frame
+    void OnNewSentence(string question)
+    {
+        m_text.text = question;
+        m_textToSpeech.RunTTS(question);
+    }
+}

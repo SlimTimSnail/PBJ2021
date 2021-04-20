3019536964908be0a9b73edc171e9ec947891d11
Kalelovil <kalelovil@gmail.com>
2021-04-21 00:06:16 +1200

    - prevent nre if no players join

--- Assets/Scripts/WordSpawner.cs
+++ Assets/Scripts/WordSpawner.cs
@@ -21,11 +21,34 @@ public class WordSpawner : MonoBehaviour
 
     private SentenceManager m_manager;
 
+    public void Awake()
+    {
+        GameController.Instance.GameStateEnterEvent += OnGameStateEntered;
+    }
+
+    public void OnGameStateEntered(GameState state)
+    {
+        switch (state)
+        {
+            case GameState.Start:
+                enabled = false;
+                break;
+            case GameState.Playing:
+                enabled = true;
+                break;
+            case GameState.Win_Level:
+                enabled = false;
+                break;
+            case GameState.Lose_Level:
+                enabled = false;
+                break;
+            default:
+                break;
+        }
+    }
 
     private void Start()
     {
-        GameController.Instance.GameStateEnterEvent += OnGameStateEntered;
-
         m_nextTime = Time.time + m_secondsInterval;
         m_manager = GameController.Instance.SentenceManager;
     }
@@ -64,26 +87,5 @@ public class WordSpawner : MonoBehaviour
 
         GameObject wordObject = Instantiate(prefab);
         wordObject.GetComponent<WordObject>().Setup(word);
-    }
-
-    public void OnGameStateEntered(GameState state)
-    {
-        switch (state)
-        {
-            case GameState.Start:
-                enabled = false;
-                break;
-            case GameState.Playing:
-                enabled = true;
-                break;
-            case GameState.Win_Level:
-                enabled = false;
-                break;
-            case GameState.Lose_Level:
-                enabled = false;
-                break;
-            default:
-                break;
-        }
     }
 }
